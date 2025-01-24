0f7cfde7b2cb450d3a521cdf0b2542fde26a5f62
Tunar <139899544+TunarJamalov@users.noreply.github.com>
2025-01-24 12:35:18 +0400

    asa
    asa

---
+++ src/__tests__/SelectCategory.test.tsx
@@ -0,0 +1,38 @@
+import SelectCategory from "../components/SelectCategory";
+import { render, cleanup, RenderResult } from "@testing-library/react";
+import { expect, afterEach, describe, it } from "vitest";
+
+afterEach(cleanup);
+
+describe("SelectCategory", () => {
+  const selectCategoryArr = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Accessibility",
+    "General CS",
+    "IT",
+    "Linux",
+    "Python",
+    "SQL"
+  ];
+
+  it("displays the Choose a Category screen", () => {
+    const { getByText }: RenderResult = render(
+      <SelectCategory selectQuiz={undefined as any} startRandomQuiz={undefined as any} />
+    );
+    expect(getByText("Choose a Category")).toBeInTheDocument();
+  });
+
+  it("displays the correct categories", () => {
+    const { getByText } = render(
+      <SelectCategory selectQuiz={undefined as any} startRandomQuiz={undefined as any} />
+    );
+    selectCategoryArr.forEach(category => {
+      expect(getByText(category)).toBeInTheDocument();
+    });
+    expect(getByText("HTML")).toBeInTheDocument();
+    expect(getByText("CSS")).toBeInTheDocument();
+    expect(getByText("JavaScript")).toBeInTheDocument();
+  });
+});
